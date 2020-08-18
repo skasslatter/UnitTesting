ffc307efcb966ad2cdfe04f36b866332f8cf9e2b
sybille <sybille@pxlwidgets-heroes.com>
2020-08-18 15:34:29 +0200

    Tests written for App component, City component, Person model

--- testing-assignment/src/app/app.component.spec.ts
+++ testing-assignment/src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -14,10 +17,26 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
+  it('should create the app', () => {
+    // const fixture = TestBed.createComponent(AppComponent);
+    // const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  it('should have the title: "testing-assignment app is running!"', () => {
+    const title = fixture.debugElement.query(By.css('.highlight-card span')).nativeElement;
+    expect(title.innerHTML).toBe('testing-assignment app is running!');
+  });
+
+  // Alternative
+  // it(`should contain 'testing-assignment app is running!'`, () => {
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.highlight-card span').textContent).toContain('testing-assignment app is running!');
+  // });
 });
---
+++ testing-assignment/src/app/model/person/person.model.spec.ts
@@ -0,0 +1,30 @@
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Person} from './person.model';
+
+describe('Person', () => {
+  let newPerson: Person;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        Person
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    newPerson = new Person('John');
+  });
+
+  it('should return the correct person name', () => {
+    expect(newPerson.sayHello()).toEqual('Hi, John');
+  });
+
+  afterEach(() => {
+    newPerson = null;
+  });
+});
